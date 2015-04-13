b61cefa419cab635b1601a3b8b0808efcdbbce14
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2015-04-13 23:01:09 +0900

    git-debimport gtk+2.0_2.24.25.orig.tar.xz

--- gtk/gtkversion.h
+++ gtk/gtkversion.h
@@ -35,9 +35,9 @@
  */
 #define GTK_MAJOR_VERSION				(2)
 #define GTK_MINOR_VERSION				(24)
-#define GTK_MICRO_VERSION				(24)
-#define GTK_BINARY_AGE					(2424)
-#define GTK_INTERFACE_AGE				(24)
+#define GTK_MICRO_VERSION				(25)
+#define GTK_BINARY_AGE					(2425)
+#define GTK_INTERFACE_AGE				(25)
 
 /* check whether a Gtk+ version equal to or greater than
  * major.minor.micro is present.
